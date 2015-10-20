4eaa063a1eda63bee4c29f6f74e84e1956a85129
BrianMSon <sonmg00@gmail.com>
2015-10-20 13:42:26 +0900

    change Title

--- routes_base/index.js
+++ routes_base/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
 	//console.log('REAL IP : ' + req.header('x-forwarded-for') || req.connection.remoteAddress);
-	res.render('index', { title: 'GGG Express' });
+	res.render('index', { title: 'Rapid Express' });
 });
 
 ////////////////////////////////////////////////////////////////////
